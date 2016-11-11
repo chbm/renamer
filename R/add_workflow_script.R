e6f94e01448fda029f8d7addf7a518121d6514c3
SQuartinAS <susanaquartin@gmail.com>
2016-11-11 17:40:39 +0000

    'add_script' function.

--- R/add_workflow_script.R
+++ R/add_workflow_script.R
@@ -2,20 +2,43 @@ library("dplyr")
 library("stringr")
 
 names <- c("utils-1_importer.R", "utils-2_exporter.R", "1_main.R", "2_explore.R")
-new_name <- "01-utils_constants.R"
+new_name <- "1-utils_constants.R"
 
-str_split(new_name, "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)")
-add_script <- function(name, wd = getwd()) {
+
+add_script <- function(name, wd = getwd(), renumber = FALSE) {
+  lookaround_int <- "(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)"
+  lookbehind_int <- "(?<=\\D)(?=\\d)"
   regex_int <- "[0-9]*[0-9]"
-  if (!grepl(regex_int , name)) {
-    file.edit(str_c(name, ".R"))
-  } else {
+  
+  if (grepl(lookaround_int , name, perl = TRUE)) {
+    split_name <- str_split(name, lookaround_int) %>% unlist()
+    number <- subset(split_name, grepl(pattern = regex_int, split_name))
+    split_name_pattern <- str_split(name, lookbehind_int) %>% unlist()
+    pattern <- ifelse(length(split_name_pattern) == 1, NA_character_, first(split_name_pattern))
+    files_wd <- list_files(number, wd, pattern)
+    file_number_grepl <- grepl(number, files_wd)
     
+    if(any(file_number_grepl)) {
+      file_conflict <- files_wd[which(file_number_grepl)]
+      if (!renumber){
+        stop(str_c("Script number in ", name, " clashes with ",
+                    file_conflict,
+                    ". Set renumber flag to TRUE to renumber scripts."))
+      } else {
+        renumber_files(number, wd, pattern)
+      }
+    }
   }
+  if(grepl(".*\\.R$", name)) {
+    file.edit(name)
+  } else {
+    file.edit(str_c(name, ".R"))
+    }
 }
 
 # auxiliary function
 renumber_files <- function(starting_number, wd = getwd(), pattern = NA_character_) {
+  print("TODO!")
 }
 
 
@@ -23,7 +46,6 @@ renumber_files <- function(starting_number, wd = getwd(), pattern = NA_character
 # auxiliary function
 list_files <- function(starting_number, wd = getwd(), pattern = NA_character_) {
   starting_vec <- str_split(starting_number, "") %>% unlist() %>% as.numeric()
-  
   if (length(starting_vec) == 1) {
     default_pattern <- str_c("[0]*[", starting_vec, "-9]|[1-9][0-9].*\\.R$")
   } else if (length(starting_vec) == 2) {
@@ -39,4 +61,6 @@ list_files <- function(starting_number, wd = getwd(), pattern = NA_character_) {
     final_pattern <- str_c("^", pattern, ".*", default_pattern)
   }
   scripts <- list.files(wd, pattern = final_pattern)
+  
+  return(scripts)
 }
